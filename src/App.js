b31f833e4aeb76b64c70e3e67680aa1cfa9cbd3d
Arvin Fernandez <arvinf07@gmail.com>
2021-06-27 15:00:43 -0400

    created all neccessary components and container files for MVP

--- src/App.js
+++ src/App.js
@@ -5,18 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <img alt="bodyfuel logo" id='App-logo' src={body_fuel}/>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img alt="bodyfuel logo" id='App-logo' src={body_fuel}/>
       </header>
     </div>
   );
