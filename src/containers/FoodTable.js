ee5019ea542b7af6cca9ee1e111a0fb87cf25118
Arvin Fernandez <arvinf07@gmail.com>
2021-06-27 15:39:41 -0400

    created container components basic  definition

--- src/containers/FoodTable.js
+++ src/containers/FoodTable.js
@@ -1,2 +1,14 @@
+import MealContainer from './MealContainer'
 
+const renderMeals = (meals = []) => {
+  return meals.map( (meal) => <MealContainer props={meal} /> )
+}
+
+export default function FoodTable(props){
+    return(
+      <table style="width:50%">
+        {renderMeals()}
+      </table>
+    )
+}
 
---
+++ src/containers/MealContainer.js
@@ -0,0 +1,19 @@
+import AddButton from '../components/AddButton'
+import FoodRow from '../components/FoodRow'
+
+const renderMealFoods = ( (mealFoods) => {
+  return mealFoods.map( (mealFood) => <FoodRow props={...mealFood}/> )
+})
+
+export default function MealContainer({name, mealFoods}){
+  return(
+    <tr className={props.name}>
+      {renderMealFoods(mealFoods)}
+      <AddButton mealName={name} />
+    </tr>
+  )
+}
+
+// add helper functions to container components
+// such as the instance and static methods that you have
+// in the VanJs version
