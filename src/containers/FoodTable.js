c0541588135b5d4dbcbdfab990fee31b22fbe694
Arvin Fernandez <arvinf07@gmail.com>
2021-07-20 19:03:11 -0400

    succesfully fetching foods and storing them in local state

--- src/containers/FoodTable.js
+++ src/containers/FoodTable.js
@@ -13,18 +13,18 @@ export default function FoodTable({meals}){
     console.log(foods)
   }, [])
 
+  function fetchFoods(){
+    fetch('http://127.0.0.1:3000/foods')
+    .then(resp => resp.json())
+    .then( foods => setFoods(foods) )
+    .catch(error => console.log('this went wrong', error))
+  }
+
   return(
     <table width={600}>
       {renderMeals(meals)}
     </table>
   )
-
-  function fetchFoods(){
-    fetch('http://127.0.0.1:3000/foods')
-    .then(resp => resp.json())
-    .then( foods => foods )
-    .catch(error => console.log('this went wrong', error))
-  }
 
 }
 
